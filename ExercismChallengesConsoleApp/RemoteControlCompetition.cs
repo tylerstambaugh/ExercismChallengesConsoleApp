b055c66f580baebce36b27ab15a91e8be8ec775c
Tyler Stambaugh <tyler.stambaugh@sevencorners.com>
2023-03-01 11:59:09 -0500

    https://exercism.org/tracks/csharp/exercises/remote-control-competition/edit

--- ExercismChallengesConsoleApp/RemoteControlCompetition.cs
+++ ExercismChallengesConsoleApp/RemoteControlCompetition.cs
@@ -1,12 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ExercismChallengesConsoleApp
+// TODO implement the IRemoteControlCar interface
+public interface IRemoteControlCar
 {
-    internal class RemoteControlCompetition
+    int DistanceTravelled { get; }
+    void Drive();
+}
+
+
+public class ProductionRemoteControlCar : IRemoteControlCar
+{
+    public int DistanceTravelled { get; private set; }
+    public int NumberOfVictories { get; set; }
+
+    public void Drive()
     {
+        DistanceTravelled += 10;
+    }
+}
+
+public class ExperimentalRemoteControlCar : IRemoteControlCar
+{
+    public int DistanceTravelled { get; private set; }
+
+    public void Drive()
+    {
+        DistanceTravelled += 20;
+    }
+}
+
+public static class TestTrack : IRemoteControlCar, IComparable<IRemoteControlCar>
+{
+    public static void Race(IRemoteControlCar car)
+    {
+        throw new NotImplementedException($"Please implement the (static) TestTrack.Race() method");
+    }
+
+    public static List<ProductionRemoteControlCar> GetRankedCars(ProductionRemoteControlCar prc1,
+        ProductionRemoteControlCar prc2)
+    {
+        throw new NotImplementedException($"Please implement the (static) TestTrack.GetRankedCars() method");
     }
 }
