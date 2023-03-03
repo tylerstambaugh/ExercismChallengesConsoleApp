553c0dfc71b6923831ea9038912f5b1637b89baa
Tyler stambaugh <11870675+tylerstambaugh@users.noreply.github.co>
2023-03-03 09:06:44 -0500

    passing: https://exercism.org/tracks/csharp/exercises/developer-privileges

--- ExercismChallengesConsoleApp/Authenticator.cs
+++ ExercismChallengesConsoleApp/Authenticator.cs
@@ -13,7 +13,37 @@ public class Authenticator
     }
 
     // TODO: Implement the Authenticator.Developers property
-    public IDictionary<string, Identity> Developers { get; }
+    public IDictionary<string, Identity> Developers 
+    { 
+        get
+        {
+            return new Dictionary<string, Identity>()
+            {
+                ["Bertrand"] = new Identity 
+                    { 
+                        Email = "bert@ex.ism",
+                        FacialFeatures = new FacialFeatures 
+                            { 
+                                EyeColor = "blue", 
+                                PhiltrumWidth = 0.8m
+                        }, 
+                        NameAndAddress = new List<string>() 
+                            { "Bertrand", "Paris", "France" } 
+                    },
+                ["Anders"] = new Identity
+                    {
+                        Email = "anders@ex.ism",
+                        FacialFeatures = new FacialFeatures 
+                            {
+                                EyeColor = "brown", 
+                                PhiltrumWidth = 0.85m
+                        }, 
+                        NameAndAddress = new List<string>() 
+                            { "Anders", "Redmond", "USA" } 
+                    }
+            };
+        }
+    }
 }
 
 //**** please do not modify the FacialFeatures class ****
