0c22dd0bfdec74e77eed172445f30431d6474c8a
Tyler Stambaugh <tyler.stambaugh@sevencorners.com>
2023-03-28 13:13:30 -0400

    passing

---
+++ ExercismChallengesConsoleApp/Isogram.cs
@@ -0,0 +1,27 @@
+﻿//https://exercism.org/tracks/csharp/exercises/isogram
+
+using System;
+using System.Collections.Generic;
+
+public static class Isogram
+{
+    public static bool IsIsogram(string word)
+    {
+        string wordToCheck = word.ToLower().Trim().Replace("-", "").Replace(" ", "");
+        Dictionary<char, int> keyValuePairs = new Dictionary<char, int>();
+        bool returnBool = true;
+        foreach (char c in wordToCheck)
+        {
+            
+            if (!keyValuePairs.ContainsKey(c))
+            {                
+                keyValuePairs.Add(c, 1);
+            }
+            else
+            {
+                returnBool = false;
+            }
+        }
+        return returnBool;
+    }
+}
