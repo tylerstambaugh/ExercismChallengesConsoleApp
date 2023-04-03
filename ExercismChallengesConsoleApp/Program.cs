b57c02882533226c945d2349efe5101e17f6f1a6
Tyler Stambaugh <tyler.stambaugh@sevencorners.com>
2023-04-03 12:43:22 -0400

    working with byte arrays, or trying to at least.

--- ExercismChallengesConsoleApp/Program.cs
+++ ExercismChallengesConsoleApp/Program.cs
@@ -3,19 +3,11 @@
 
 using ExercismChallengesConsoleApp;
 
-var troof = Isogram.IsIsogram("Emily Jung Schwartzkopf");
-Console.WriteLine(troof);
+var returnBytes = TelemetryBuffer.ToBuffer(Int32.MaxValue);
+Console.WriteLine(  returnBytes.ToList());
+for(int i = 0; i < returnBytes.Length; i++)
+{
+    Console.WriteLine(returnBytes[i]);
+}
 
-
-//var dt = new DateTime(2030, 07, 25, 13, 45, 0);
-//var tzi = TimeZoneInfo.Local;
-//var offset = tzi.GetUtcOffset(dt);
-//Console.WriteLine($"dt + offest= {dt + offset}, appt local time = {Appointment.ShowLocalTime(dt)}");
-
-//var scheduledAppt = Appointment.Schedule("7/25/2019 08:45:00", Location.NewYork);
-//var scheduledAppt2 = Appointment.Schedule("7/25/2019 13:45:00", Location.London);
-//var scheduledAppt3 = Appointment.Schedule("7/25/2019 14:45:00", Location.London);
-
-//Console.WriteLine($"1: {scheduledAppt}, 2: {scheduledAppt2}, 3: {scheduledAppt3}");
-
-//Console.ReadLine();
+Console.ReadLine();
