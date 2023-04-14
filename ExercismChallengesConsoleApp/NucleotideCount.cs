8c4a316114238fefdbba3b0a4a95b2b0833d8042
Tyler Stambaugh <tyler.stambaugh@sevencorners.com>
2023-04-14 06:50:36 -0400

    nucleotide count passing

---
+++ ExercismChallengesConsoleApp/NucleotideCount.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class NucleotideCount
+{
+    public static IDictionary<char, int> Count(string sequence)
+    {
+        Dictionary<char, int> dic = (Dictionary<char, int>)CreateEmtpyDictionary();
+
+        string sequenceToCheck = sequence.ToUpper().Trim();
+
+        foreach (char c in sequenceToCheck)
+        {
+            if(dic.ContainsKey(c))
+                dic[c] += 1;
+
+        }
+
+        return dic;
+    }
+
+    private static  IDictionary<char, int> CreateEmtpyDictionary()
+    {
+        return new Dictionary<char, int>()
+        {
+            { 'A', 0 },
+            { 'C', 0 },
+            { 'G', 0 },
+            { 'T' ,0 }
+        };
+    }
+}
