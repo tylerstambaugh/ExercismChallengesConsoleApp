2ef3619ce8e41b126fd0f6e9a4a68ddc4a114c7c
Tyler Stambaugh <tyler.stambaugh@sevencorners.com>
2023-07-12 20:24:49 -0400

    collatz conjecture algo

--- ExercismChallengesConsoleApp/TelemetryBuffer.cs
+++ ExercismChallengesConsoleApp/TelemetryBuffer.cs
@@ -1,23 +1,23 @@
 ﻿using System;
 
-public static class TelemetryBuffer
-{
-    public static byte[] ToBuffer(long reading)
-    {
-        byte[] returnArray = new byte[];
-        if (reading > 0 && reading < 65535)
-        {
-            returnArray =  BitConverter.GetBytes((int)reading);
-            return returnArray;
-        }
-        else
-        {
-            return BitConverter.GetBytes(reading);
-        }
-    }
+//public static class TelemetryBuffer
+//{
+//    public static byte[] ToBuffer(long reading)
+//    {
+//        byte[] returnArray = new byte[];
+//        if (reading > 0 && reading < 65535)
+//        {
+//            returnArray =  BitConverter.GetBytes((int)reading);
+//            return returnArray;
+//        }
+//        else
+//        {
+//            return BitConverter.GetBytes(reading);
+//        }
+//    }
 
-    public static long FromBuffer(byte[] buffer)
-    {
-       return BitConverter.ToInt64(buffer, 0);
-    }
-}
+//    public static long FromBuffer(byte[] buffer)
+//    {
+//       return BitConverter.ToInt64(buffer, 0);
+//    }
+//}
