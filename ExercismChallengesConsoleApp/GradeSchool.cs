c9f5d136e0b5b421b777ddb3023af86ff148294d
Tyler Stambaugh <tyler.stambaugh@sevencorners.com>
2023-07-24 20:02:01 -0400

    grade school passing

---
+++ ExercismChallengesConsoleApp/GradeSchool.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+
+public class GradeSchool
+{
+
+    private Dictionary<string, int> RosterDict = new Dictionary<string, int>();
+
+    public bool Add(string student, int grade)
+    {
+        if (RosterDict.ContainsKey(student))
+            return false;
+        else
+        {
+            RosterDict.Add(student, grade);
+            return true;
+        }
+
+    }
+
+    public IEnumerable<string> Roster()
+    {
+        return RosterDict.OrderBy(y => y.Key).OrderBy(x => x.Value).Select(x => x.Key);
+    }
+
+    public IEnumerable<string> Grade(int grade)
+    {
+        return RosterDict.Where(x => x.Value == grade).OrderBy(y => y.Key).Select(x => x.Key);
+    }
+}
