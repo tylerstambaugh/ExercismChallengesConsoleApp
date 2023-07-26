6a915dba35fd897a566f7916d1b5ab28fb59c95e
Tyler Stambaugh <tyler.stambaugh@sevencorners.com>
2023-07-26 14:50:59 -0400

    faltten array passing

---
+++ ExercismChallengesConsoleApp/FlattenArray.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+
+public static class FlattenArray
+{
+    public static List<int> returnArray = new List<int>();
+    public static IEnumerable Flatten(IEnumerable input)
+    {
+        foreach (var item in input)
+        {
+            if (item is object[] nestedArray)
+            {
+                Flatten(nestedArray);
+            }
+            else if(item != null && item is int)
+            {
+                returnArray.Add((int)item);
+            } 
+        }
+        return returnArray;
+    }
+}
