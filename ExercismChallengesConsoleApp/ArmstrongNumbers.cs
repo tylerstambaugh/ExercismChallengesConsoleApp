1bc81771998e08725053987d6fef782813160e64
Tyler stambaugh <11870675+tylerstambaugh@users.noreply.github.co>
2024-05-29 20:46:17 -0400

    ArmstrongNumber passing (first try)

---
+++ ExercismChallengesConsoleApp/ArmstrongNumbers.cs
@@ -0,0 +1,22 @@
+﻿//https://exercism.org/tracks/csharp/exercises/armstrong-numbers
+namespace ExercismChallengesConsoleApp
+{
+
+    public static class ArmstrongNumbers
+    {
+        public static bool IsArmstrongNumber(int number)
+        {
+            int charCount = number.ToString().Length;
+            string numberString = number.ToString();
+            double total = 0;
+
+            for(int i = 0; i < charCount; i++)
+            {
+                int digit = int.Parse(numberString[i].ToString());
+                total += Math.Pow(digit, charCount);
+            }
+
+            return total == number;
+        }
+    }
+}
