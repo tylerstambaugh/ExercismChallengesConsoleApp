41a71a34c21e958bb8cfd0947836481ea0c0b8c2
Tyler stambaugh <11870675+tylerstambaugh@users.noreply.github.co>
2024-06-09 21:22:02 -0400

    accounting

--- ExercismChallengesConsoleApp/Program.cs
+++ ExercismChallengesConsoleApp/Program.cs
@@ -2,6 +2,12 @@
 
 
 using ExercismChallengesConsoleApp;
+using System.Text.Json;
+var url = "/iou";
+var payload = "{\"lender\":\"Adam\",\"borrower\":\"Bob\",\"amount\":4}";
+var database = "[{\"name\":\"Adam\",\"owes\":{\"Bob\":3},\"owed_by\":{},\"balance\":-3},{\"name\":\"Bob\",\"owes\":{},\"owed_by\":{\"Adam\":3},\"balance\":3}]";
+var sut = new RestApi(database);
+var actual = sut.Post(url, payload);
 
-
-var result = PerfectNumbers.Classify(6);
+Console.WriteLine(actual);
+Console.ReadLine();
