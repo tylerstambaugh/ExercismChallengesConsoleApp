488a51860b3cfe0f194868d123458b14220a7e0a
Tyler stambaugh <11870675+tylerstambaugh@users.noreply.github.co>
2024-06-19 21:25:28 -0400

    starting another one

--- ExercismChallengesConsoleApp/Authenticator.cs
+++ ExercismChallengesConsoleApp/Authenticator.cs
@@ -1,62 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 public class Authenticator
 {
-    // TODO: Implement the Authenticator.Admin property
+    private class EyeColor
+    {
+        const string Blue = "blue";
+        const string Green = "green";
+        const string Brown = "brown";
+        const string Hazel = "hazel";
+        const string Grey = "grey";
+    }
+
+    public Authenticator(Identity admin)
+    {
+        this.admin = admin;
+    }
+
+    private Identity admin { get;  }
+
+    private readonly IDictionary<string, Identity> developers
+        = new Dictionary<string, Identity>
+        {
+            ["Bertrand"] = new Identity
+            {
+                Email = "bert@ex.ism",
+                EyeColor = "blue"
+            },
+
+            ["Anders"] = new Identity
+            {
+                Email = "anders@ex.ism",
+                EyeColor = "brown"
+            }
+        };
+
     public Identity Admin
     {
-        get
-        {
-            return new Identity {Email = "admin@ex.ism", FacialFeatures = new FacialFeatures { EyeColor = "green",  PhiltrumWidth = 0.9m }, NameAndAddress = new List<string>() { "Chanakya", "Mumbai", "India" } };
-        }
+        get { return admin; }
     }
 
-    // TODO: Implement the Authenticator.Developers property
-    public IDictionary<string, Identity> Developers 
-    { 
-        get
-        {
-            return new Dictionary<string, Identity>()
-            {
-                ["Bertrand"] = new Identity 
-                    { 
-                        Email = "bert@ex.ism",
-                        FacialFeatures = new FacialFeatures 
-                            { 
-                                EyeColor = "blue", 
-                                PhiltrumWidth = 0.8m
-                        }, 
-                        NameAndAddress = new List<string>() 
-                            { "Bertrand", "Paris", "France" } 
-                    },
-                ["Anders"] = new Identity
-                    {
-                        Email = "anders@ex.ism",
-                        FacialFeatures = new FacialFeatures 
-                            {
-                                EyeColor = "brown", 
-                                PhiltrumWidth = 0.85m
-                        }, 
-                        NameAndAddress = new List<string>() 
-                            { "Anders", "Redmond", "USA" } 
-                    }
-            };
-        }
+    public IDictionary<string, Identity> GetDevelopers()
+    {
+        return developers;
     }
 }
 
-//**** please do not modify the FacialFeatures class ****
-public class FacialFeatures
-{
-    public string EyeColor { get; set; }
-    public decimal PhiltrumWidth { get; set; }
-}
-
-//**** please do not modify the Identity class ****
-public class Identity
+public struct Identity
 {
     public string Email { get; set; }
-    public FacialFeatures FacialFeatures { get; set; }
-    public IList<string> NameAndAddress { get; set; }
+
+    public string EyeColor { get; set; }
 }
