49a36c888f0f57a06d22df13f4158f12c0e506b1
Tyler stambaugh <11870675+tylerstambaugh@users.noreply.github.co>
2024-09-06 22:08:10 -0400

    passing

--- ExercismChallengesConsoleApp/CurrencyStruct.cs
+++ ExercismChallengesConsoleApp/CurrencyStruct.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -21,37 +22,128 @@ namespace ExercismChallengesConsoleApp
 
         public static bool operator ==(CurrencyAmount currencyAmountA, CurrencyAmount currencyAmountB)
         {
-            return true;
+            if (currencyAmountA.currency == currencyAmountB.currency)
+            {
+                {
+                    return (currencyAmountA.amount == currencyAmountB.amount);
+                }
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
         }
 
         public static bool operator !=(CurrencyAmount currencyAmountA, CurrencyAmount currencyAmountB)
         {
-            return true;
+            if (currencyAmountA.currency == currencyAmountB.currency)
+            {
+                {
+                    return (currencyAmountA.amount != currencyAmountB.amount);
+                }
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
         }
 
         // TODO: implement comparison operators
         public static bool operator <(CurrencyAmount currencyAmountA, CurrencyAmount currencyAmountB)
         {
-            return true;
+            if (currencyAmountA.currency == currencyAmountB.currency)
+            {
+                {
+                    return (currencyAmountA.amount < currencyAmountB.amount);
+                }
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
         }
 
         public static bool operator >(CurrencyAmount currencyAmountA, CurrencyAmount currencyAmountB)
         {
-            return true;
+            if (currencyAmountA.currency == currencyAmountB.currency)
+            {
+                {
+                    return (currencyAmountA.amount > currencyAmountB.amount);
+                }
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
         }
 
         // TODO: implement arithmetic operators
-        public static bool operator +(CurrencyAmount currencyAmountA, CurrencyAmount currencyAmountB)
+        public static decimal operator +(CurrencyAmount currencyAmountA, CurrencyAmount currencyAmountB)
         {
-            return true;
+            if (currencyAmountA.currency == currencyAmountB.currency)
+            {
+                {
+                    return (currencyAmountA.amount + currencyAmountB.amount);
+                }
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
         }
 
-        public static bool operator -(CurrencyAmount currencyAmountA, CurrencyAmount currencyAmountB)
+        public static decimal operator - (CurrencyAmount currencyAmountA, CurrencyAmount currencyAmountB)
         {
-            return true;
+            if (currencyAmountA.currency == currencyAmountB.currency)
+            {
+                {
+                    return (currencyAmountA.amount - currencyAmountB.amount);
+                }
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
+        }
+
+        public static decimal operator * (CurrencyAmount currencyAmountA, CurrencyAmount currencyAmountB)
+        {
+            if (currencyAmountA.currency == currencyAmountB.currency)
+            {
+                {
+                    return (currencyAmountA.amount * currencyAmountB.amount);
+                }
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
+        }
+
+        public static decimal operator / (CurrencyAmount currencyAmountA, CurrencyAmount currencyAmountB)
+        {
+            if (currencyAmountA.currency == currencyAmountB.currency)
+            {
+                {
+                    return (currencyAmountA.amount / currencyAmountB.amount);
+                }
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
         }
 
         // TODO: implement type conversion operators
+        public static explicit operator double(CurrencyAmount currencyAmount)
+        {
+            return (double) currencyAmount.amount;
+        }
+
+        public static implicit operator decimal(CurrencyAmount currencyAmount)
+        {
+            return (decimal)currencyAmount.amount;
+        }
     }
     
 }
