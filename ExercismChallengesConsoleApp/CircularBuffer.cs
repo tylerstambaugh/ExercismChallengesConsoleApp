a823639e8ea922ce53d33a3256725014400fdfa5
Tyler stambaugh <11870675+tylerstambaugh@users.noreply.github.co>
2024-09-19 21:27:20 -0400

    like 1/3 passing

--- ExercismChallengesConsoleApp/CircularBuffer.cs
+++ ExercismChallengesConsoleApp/CircularBuffer.cs
@@ -2,19 +2,42 @@
 {
     public class CircularBuffer<T>
     {
+
+        public int readIndex;
+        public int writeIndex;
+        public T[] buffer;
         public CircularBuffer(int capacity)
         {
-            throw new NotImplementedException("You need to implement this method.");
+            buffer = new T[capacity];
+            readIndex = 0;
+            writeIndex = 0;
         }
 
         public T Read()
         {
-            throw new NotImplementedException("You need to implement this method.");
+            if(writeIndex == 0)            
+                throw new InvalidOperationException();
+            
+            T result = buffer[readIndex];
+
+            if (readIndex == buffer.Length - 1)
+            {
+                readIndex = 0;  
+            }
+            else
+            {
+                readIndex++;
+            }
+            
+            return result;
         }
 
         public void Write(T value)
         {
-            throw new NotImplementedException("You need to implement this method.");
+            if(writeIndex == buffer.Length)
+                throw new InvalidOperationException();
+
+                buffer[writeIndex++] = value;
         }
 
         public void Overwrite(T value)
